4c1e26991c3a319e9fc21de9274ef3372e36ca13
Art-1313 <71838986+Art-1313@users.noreply.github.com>
2020-09-25 01:57:18 +0300

    Create ex3.py

---
+++ lab3/ex3.py
@@ -0,0 +1,24 @@
+from turtle import *
+from numpy import *
+
+def addnum(num:int):
+    inp = open('ex3_sup.txt', 'r').readlines()
+    s = inp[num].rstrip() #в строке с номеров num содержится алгоритм для цифры num
+    commlist = s.split(' -> ')
+    for command in commlist:
+        eval(command)
+
+
+def drawindex(index:float):
+    for num in index:
+        addnum(int(num))
+
+
+shape('turtle')
+speed(8)
+color('blue','blue')
+width(3)
+left(90)
+index = '141700'
+drawindex(index) 
+done(
