09c221539f06bdeeeeaea4a82168d1b2cd984835
Art-1313 <gromov.ak@phystech.edu>
2020-10-28 23:32:27 +0300

    add exit function

--- lab5/gun.py
+++ lab5/gun.py
@@ -2,6 +2,8 @@ import pygame as pg
 import  numpy as np
 
 pg.init()
+FPS = 60
+SCREEN_SIZE = (800,600)
 
 class Gun():
     pass
@@ -13,7 +15,27 @@ class Bullet():
     pass
 
 class Manager():
-    pass
+
+    def process(self, events):
+        done = self.event_handler(events)
+        return done
+
+    def event_handler(self, events):
+        done = False
+        for event in events:
+            if event.type == pg.QUIT:
+                done = True
+        return done;
+
+
+clock = pg.time.Clock()
+done = False
+screen = pg.display.set_mode(SCREEN_SIZE)
+mgr = Manager()
+
+while not done:
+    clock.tick(FPS)
+    done = mgr.process(pg.event.get())
 
 pg.quit()
 
