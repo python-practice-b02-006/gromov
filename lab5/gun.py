4d15f2d1de2108b6519a8a6369c21c0666cdf47b
Art-1313 <gromov.ak@phystech.edu>
2020-10-28 12:00:05 +0300

    create needed structure

---
+++ lab5/gun.py
@@ -0,0 +1,19 @@
+import pygame as pg
+import  numpy as np
+
+pg.init()
+
+class Gun():
+    pass
+
+class Target():
+    pass
+
+class Bullet():
+    pass
+
+class Manager():
+    pass
+
+pg.quit()
+
