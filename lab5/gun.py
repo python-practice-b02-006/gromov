1f506df2a0363a550539993635922eefebcc1278
Art-1313 <gromov.ak@phystech.edu>
2020-10-30 01:09:45 +0300

    add doc strings

--- lab5/gun.py
+++ lab5/gun.py
@@ -13,12 +13,24 @@ RED = (255, 0, 0)
 
 
 def rand_color():
+    """
+    set a random color
+    Returns
+    -------
+    tuple(R,G,B)
+    """
     return (randint(20, 255), randint(20, 255), randint(20, 255))
 
 
 class Gun():
+    """
+    Gun class. Manages it's renderring, movement and striking.
+    """
 
     def __init__(self):
+        """
+        Constructor method. Sets coordinate, direction, minimum and maximum power and color of the gun.
+        """
         self.x = SCREEN_SIZE[0] / 2
         self.y = SCREEN_SIZE[1]
         self.size = 40
@@ -30,11 +42,23 @@ class Gun():
         self.step_active = False
 
     def draw_body(self):
+        """
+        Draw the body of the gun
+        Returns
+        -------
+        None
+        """
         pg.draw.rect(screen, GREEN,
                      (int(self.x - self.size / 2), int(self.y - self.size),
                       int(self.size), int(self.size)), 0)
 
     def draw_huy(self):
+        """
+        Draw the huy of the gun
+        Returns
+        -------
+        None
+        """
         angle = np.arctan2((pg.mouse.get_pos()[0] - self.x),
                            -pg.mouse.get_pos()[1] + SCREEN_SIZE[1] - 5 * self.size / 6)
         pg.draw.line(screen, GREEN, (int(self.x), int(self.y - 5 * self.size / 6)),
@@ -42,10 +66,22 @@ class Gun():
                      5)
 
     def draw(self):
+        """
+        Draw the whole gun
+        Returns
+        -------
+        None
+        """
         self.draw_huy()
         self.draw_body()
 
     def strike(self):
+        """
+        Create a bullet according to actual position and power
+        Returns
+        -------
+        Element of class Bullet
+        """
         angle = np.arctan2((pg.mouse.get_pos()[0] - self.x),
                            -pg.mouse.get_pos()[1] + SCREEN_SIZE[1] - 5 * self.size / 6)
         bullet = Bullet(
@@ -56,9 +92,21 @@ class Gun():
         return bullet
 
     def activate(self):
+        """
+        Make gun activated
+        Returns
+        -------
+        None
+        """
         self.active = True
 
     def gain(self):
+        """
+        Set the gun power
+        Returns
+        -------
+        None
+        """
         if self.active and self.speed <= self.speed_max:
             self.speed += 2 / self.speed
         if self.step_active:
@@ -68,16 +116,38 @@ class Gun():
                 self.x -= 1
 
     def move(self, direct):
+        """
+        Control gan's movments
+        Parameters
+        ----------
+        direct = bool (set a direction: True = Right)
+
+        Returns
+        -------
+        None
+        """
         self.dir = direct
         self.step_active = True
 
     def stop(self):
+        """
+        Make gun to stop
+        Returns
+        -------
+        None
+        """
         self.step_active = False
 
 
 class Target():
+    """
+    Target class. Creates target, manages it's rendering and collision with a bullet event.
+    """
 
     def __init__(self):
+        """
+        Constructor method. Sets coordinate, color and radius of the target.
+        """
         self.size = 30
         self.dir = randint(0, 1)
         if self.dir == 0:
@@ -92,6 +162,12 @@ class Target():
         self.fired = False
 
     def move(self):
+        """
+        Control target's movements
+        Returns
+        -------
+        None
+        """
         self.x += self.speed
         if self.dir == 0:
             if self.x > SCREEN_SIZE[0] + 2 * self.size:
@@ -101,16 +177,42 @@ class Target():
                 self.is_alive = False
 
     def draw(self):
+        """
+        Draw a target
+        Returns
+        -------
+        None
+        """
         pg.draw.rect(screen, self.color, (self.x, self.y, 2 * self.size, self.size), 0)
 
     def check_collision(self, bullet):
+        """
+        Control collision event
+        Parameters
+        ----------
+        bullet = Bullet()
+
+        Returns
+        -------
+        bool
+        """
         if self.y + self.size > bullet.coord[1] > self.y and self.x + 2 * self.size > bullet.coord[0] > self.x:
             return True
         else:
             return False
 
     def strike(self, gun):
-        if self.x + self.size/2 <= gun.x + gun.size/2 <= self.x + 3*self.size/2 and not self.fired:
+        """
+        Control strike event
+        Parameters
+        ----------
+        gun = Gun()
+
+        Returns
+        -------
+        bool
+        """
+        if self.x + self.size / 2 <= gun.x + gun.size / 2 <= self.x + 3 * self.size / 2 and not self.fired:
             self.fired = True
             return True
         else:
@@ -118,8 +220,14 @@ class Target():
 
 
 class Bullet():
+    """
+    The bullet class. Creates a bullet, controls it's movement and implement it's rendering.
+    """
 
     def __init__(self, coord, vel, rad=5, color=None):
+        """
+        Constructor method. Initializes bullet's parameters and initial values.
+        """
         self.coord = coord
         self.vel = vel
         if color == None:
@@ -129,6 +237,16 @@ class Bullet():
         self.is_alive = True
 
     def move(self, grav):
+        """
+        Control bullet's movments
+        Parameters
+        ----------
+        grav = float
+
+        Returns
+        -------
+        None
+        """
         self.vel[1] += grav
         for i in range(2):
             self.coord[i] += self.vel[i]
@@ -139,12 +257,24 @@ class Bullet():
             self.is_alive = False
 
     def draw(self):
+        """
+        Draw a bullet
+        Returns
+        -------
+        None
+        """
         pg.draw.circle(screen, self.color, list(map(int, self.coord)), self.rad)
 
 
 class Bomb():
+    """
+    The bomb class. Creates a bomb, controls it's movement and implement it's rendering.
+    """
 
     def __init__(self, y, x, speed):
+        """
+        Constructor method. Initializes bomb's parameters and initial values.
+        """
         self.size = 20
         self.y = y
         self.speed = speed
@@ -152,26 +282,54 @@ class Bomb():
         self.is_alive = True
 
     def move(self):
+        """
+        Control bomb's movements
+        Returns
+        -------
+        None
+        """
         self.y += self.speed
         if self.y > SCREEN_SIZE[1]:
             self.is_alive = False
 
     def draw(self):
-        pg.draw.rect(screen, RED, (int(self.x) - int(3*self.size/2),
+        """
+        Draw a bomb
+        Returns
+        -------
+        None
+        """
+        pg.draw.rect(screen, RED, (int(self.x) - int(3 * self.size / 2),
                                    int(self.y), int(self.size), int(self.size)), 0)
 
     def check_collision(self, gun):
+        """
+        Check a collision event
+        Parameters
+        ----------
+        gun = Gun
+
+        Returns
+        -------
+        bool
+        """
         if self.y + self.size > gun.y - gun.size \
-                and (self.x - self.size/2 > gun.x - gun.size/2
-                     and gun.x + gun.size/2 > self.x - 3*self.size/2):
+                and (self.x - self.size / 2 > gun.x - gun.size / 2
+                     and gun.x + gun.size / 2 > self.x - 3 * self.size / 2):
             return True
         else:
             return False
 
 
 class ScoreTable():
+    """
+    Score table class
+    """
 
     def __init__(self, t_destr=0, lives=5):
+        """
+        Set initial scores and lives
+        """
         self.t_destr = t_destr
         self.lives = lives
         self.font = pg.font.SysFont("dejavusansmono", 25)
@@ -180,6 +338,16 @@ class ScoreTable():
         return self.t_destr
 
     def draw(self, pow):
+        """
+        Print scores, lives, power
+        Parameters
+        ----------
+        pow = int (the gun power)
+
+        Returns
+        -------
+        None
+        """
         pg.draw.rect(screen, BLACK, (SCREEN_SIZE[0], 0, 150, SCREEN_SIZE[1]), 0)
         pg.draw.line(screen, WHITE, (SCREEN_SIZE[0], 0), (SCREEN_SIZE[0], SCREEN_SIZE[1]), 5)
         score_surf = []
@@ -192,13 +360,25 @@ class ScoreTable():
         screen.blit((self.font.render("POWER".format(self.t_destr), True, WHITE)), [845, 570])
 
     def loose(self):
+        """
+        Print final message
+        Returns
+        -------
+        None
+        """
         self.font = pg.font.SysFont("dejavusansmono", 100)
         screen.blit((self.font.render("YOU LOSE".format(self.t_destr), True, WHITE)), [300, 250])
 
 
 class Manager():
+    """
+    Control all events and objects
+    """
 
     def __init__(self):
+        """
+        Initialize objects and necessary variables
+        """
         self.gun = Gun()
         self.bullets = []
         self.targets = []
@@ -209,10 +389,26 @@ class Manager():
         self.b_spd = 3
 
     def new_mission(self):
+        """
+        Start a new mission
+        Returns
+        -------
+        None
+        """
         for i in range(self.n_targets):
             self.targets.append(Target())
 
     def process(self, events):
+        """
+        Rule process
+        Parameters
+        ----------
+        events = pg.event
+
+        Returns
+        -------
+        bool
+        """
         done = self.event_handler(events)
         self.move()
         self.collide()
@@ -222,6 +418,16 @@ class Manager():
         return done
 
     def event_handler(self, events):
+        """
+        Read events
+        Parameters
+        ----------
+        events = pg.event
+
+        Returns
+        -------
+        bool
+        """
         done = False
         for event in events:
             if event.type == pg.QUIT:
@@ -245,6 +451,12 @@ class Manager():
         return done
 
     def draw(self):
+        """
+        Draw all objects
+        Returns
+        -------
+        None
+        """
         self.gun.draw()
         for bullet in self.bullets:
             bullet.draw()
@@ -255,6 +467,12 @@ class Manager():
         self.table.draw(self.gun.speed)
 
     def move(self):
+        """
+        Move all objects
+        Returns
+        -------
+        None
+        """
         self.gun.gain()
         dead_bullets = []
         for i, bullet in enumerate(self.bullets):
@@ -281,6 +499,12 @@ class Manager():
             self.bombs.pop(i)
 
     def collide(self):
+        """
+        Check interactions between objects. Finish the game if you lose
+        Returns
+        -------
+        None
+        """
         collisions = []
         targets_c = []
         for i, bullet in enumerate(self.bullets):
@@ -308,13 +532,12 @@ class Manager():
                 pg.quit()
 
 
-
 clock = pg.time.Clock()
 DONE = False
 screen = pg.display.set_mode((SCREEN_SIZE[0] + 150, SCREEN_SIZE[1]))
 mgr = Manager()
 
-while not DONE:
+while not DONE:  # the main cycle of the program
     clock.tick(FPS)
     screen.fill(BLACK)
     DONE = mgr.process(pg.event.get())
