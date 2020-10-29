2c2274c66ceefa4f77e214ce994e7291d5a32b2f
Art-1313 <gromov.ak@phystech.edu>
2020-10-29 16:28:02 +0300

    make gun shot

--- lab5/gun.py
+++ lab5/gun.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import numpy as np
-import keyboard as kb
+from random import *
 
 pg.init()
 pg.font.init()
@@ -8,6 +8,9 @@ FPS = 60
 SCREEN_SIZE = (800, 600)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
+
+def rand_color():
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
 
 
 class Gun():
@@ -17,6 +20,8 @@ class Gun():
         self.y = SCREEN_SIZE[1]
         self.size = 40
         self.r = 2 * self.size
+        self.speed = 1
+        self.active = False
 
     def draw_body(self):
         pg.draw.rect(screen, GREEN,
@@ -24,34 +29,65 @@ class Gun():
                       int(self.size),  int(self.size)), 0)
 
     def draw_huy(self):
-        angle = np.arctan((pg.mouse.get_pos()[0] - self.x) / (-pg.mouse.get_pos()[1]+799))
+        angle = np.arctan2((pg.mouse.get_pos()[0] - self.x), -pg.mouse.get_pos()[1] + SCREEN_SIZE[1] - 5*self.size/6)
         pg.draw.line(screen, GREEN, (int(self.x), int(self.y - 5*self.size/6)),
-                     (int(self.x + np.sin(angle)*self.r),
-                      int(self.y - np.cos(angle)*self.r)),
-                     10)
+                     (int(self.x + np.sin(angle)*self.r), int(self.y - 5*self.size/6 - np.cos(angle)*self.r)), 5)
 
     def draw(self):
         self.draw_huy()
         self.draw_body()
+
+    def strike(self):
+        angle = np.arctan2((pg.mouse.get_pos()[0] - self.x), -pg.mouse.get_pos()[1] + SCREEN_SIZE[1] - 5*self.size/6)
+        bullet = Bullet([int(self.x + np.sin(angle)*self.r), int(self.y - 5*self.size/6 - np.cos(angle)*self.r)],
+                        [int(np.sin(angle)*self.speed), int(-np.cos(angle)*self.speed)])
+        self.active = False
+        self.speed = 1
+        return bullet
+
+    def activate(self):
+        self.active = True
+
+    def gain(self):
+        if self.active:
+            self.speed += 0.2
 
 
 class Target:
     pass
 
 
-class Bullet:
-    pass
+class Bullet():
+
+    def __init__(self, coord, vel, rad=20, color=None):
+        self.coord = coord
+        self.vel = vel
+        if color == None:
+            color = rand_color()
+        self.color = color
+        self.rad = rad
+        self.is_alive = True
+
+    def move(self, grav = 0.2):
+        self.vel[1] += grav
+        for i in range(2):
+            self.coord[i] += self.vel[i]
+
+    def draw(self):
+        pg.draw.circle(screen, self.color, list(map(int, self.coord)), self.rad)
 
 
 class Manager():
 
     def __init__(self):
         self.gun = Gun()
+        self.bullets = []
 
     def process(self, events):
         done = self.event_handler(events)
 
-        self.gun.draw()
+        self.draw()
+        self.move()
 
         return done
 
@@ -60,7 +96,23 @@ class Manager():
         for event in events:
             if event.type == pg.QUIT:
                 done = True
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.gun.activate()
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_SPACE:
+                    self.bullets.append(self.gun.strike())
         return done
+
+    def draw(self):
+        self.gun.draw()
+        for bullet in self.bullets:
+            bullet.draw()
+
+    def move(self):
+        self.gun.gain()
+        for bullet in self.bullets:
+            bullet.move()
 
 
 clock = pg.time.Clock()
